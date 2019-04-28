3b7e743183e6ddd50a94b93ba340ce8d4c4895fb
kkw <scratch314@gmail.com>
2019-04-29 00:54:11 +0900

    PTPlayerController 클래스 추가

---
+++ PT/Source/PT/PTPlayerController.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PTPlayerController.h"
+
+APTPlayerController::APTPlayerController(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer)
+{
+}
---
+++ PT/Source/PT/PTPlayerController.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PTPlayerController.generated.h"
+
+UCLASS()
+class PT_API APTPlayerController : public APlayerController
+{
+	GENERATED_UCLASS_BODY()
+};
