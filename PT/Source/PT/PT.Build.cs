f0ac27474fe94e9c8769f97bafbc0dfa9454924c
kkw <scratch314@gmail.com>
2019-04-28 22:35:44 +0900

    OptimizeCode = CodeOptimization.Never; => 추가

--- PT/Source/PT/PT.Build.cs
+++ PT/Source/PT/PT.Build.cs
@@ -8,6 +8,15 @@ public class PT : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+        if (Target.Type == TargetType.Editor)
+        {
+            OptimizeCode = CodeOptimization.Never;
+
+            //유니티빌드를 off시킴.
+            //유니티빌드를 on시키면, cpp에서 include돼야 할 header가 없어도 컴파일 되는 경우가 발생한다. 이걸 막기 위함
+            //bFasterWithoutUnity = true;
+        }
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 	}
 }
