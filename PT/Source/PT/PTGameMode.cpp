cccc339721035c8898c5b92fabb38b30bc11d191
kkw <scratch314@gmail.com>
2019-05-08 02:02:55 +0900

    Character 폴더로 이동 (Character 파일들)

--- PT/Source/PT/PTGameMode.cpp
+++ PT/Source/PT/PTGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "PTGameMode.h"
-#include "PTCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 #include "PTPlayerController.h"
 #include "PTHUD.h"
